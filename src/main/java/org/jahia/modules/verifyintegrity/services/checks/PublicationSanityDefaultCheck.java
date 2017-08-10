d6e43ed016f8353dcc5eafa7a384d407298316c7
Damien <dsaulnier@jahia.com>
2017-08-10 18:36:41 +0200

    Implemented a check for nodes that would have no pending modification, but a different path in live and default (fix to be implemented)

--- src/main/java/org/jahia/modules/verifyintegrity/services/checks/PublicationSanityDefaultCheck.java
+++ src/main/java/org/jahia/modules/verifyintegrity/services/checks/PublicationSanityDefaultCheck.java
@@ -1,7 +1,9 @@
 package org.jahia.modules.verifyintegrity.services.checks;
 
+import org.apache.commons.lang.StringUtils;
 import org.jahia.modules.verifyintegrity.services.ContentIntegrityCheck;
 import org.jahia.modules.verifyintegrity.services.ContentIntegrityError;
+import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionFactory;
 import org.jahia.services.content.JCRSessionWrapper;
 import org.slf4j.Logger;
@@ -11,6 +13,11 @@ import javax.jcr.ItemNotFoundException;
 import javax.jcr.Node;
 import javax.jcr.RepositoryException;
 
+import java.util.Calendar;
+
+import static org.jahia.api.Constants.JAHIAMIX_LASTPUBLISHED;
+import static org.jahia.api.Constants.JCR_LASTMODIFIED;
+import static org.jahia.api.Constants.LASTPUBLISHED;
 import static org.jahia.api.Constants.LIVE_WORKSPACE;
 import static org.jahia.api.Constants.PUBLISHED;
 
@@ -19,27 +26,56 @@ public class PublicationSanityDefaultCheck extends ContentIntegrityCheck impleme
     private static final Logger logger = LoggerFactory.getLogger(PublicationSanityDefaultCheck.class);
 
 
-    private enum ErrorType {NO_LIVE_NODE}
+    private enum ErrorType {NO_LIVE_NODE, DIFFERENT_PATH}
 
     @Override
     public ContentIntegrityError checkIntegrityBeforeChildren(Node node) {
         try {
             final JCRSessionWrapper liveSession = JCRSessionFactory.getInstance().getCurrentSystemSession(LIVE_WORKSPACE, null, null);
             if (node.hasProperty(PUBLISHED) && node.getProperty(PUBLISHED).getBoolean()) {
+                final JCRNodeWrapper liveNode;
                 try {
-                    liveSession.getNodeByIdentifier(node.getIdentifier());
-                    return null;
+                    liveNode = liveSession.getNodeByIdentifier(node.getIdentifier());
                 } catch (ItemNotFoundException infe) {
                     final String msg = "Found a node flagged as published, but no corresponding live node exists";
                     final ContentIntegrityError error = ContentIntegrityError.createError(node, null, msg, this);
                     error.setExtraInfos(ErrorType.NO_LIVE_NODE);
                     return error;
                 }
+                if (!hasPendingModifications(node)) {
+                    if (!StringUtils.equals(node.getPath(), liveNode.getPath())) {
+                        final String msg = "Found a published node, with no pending modifications, but the path in live is different";
+                        final ContentIntegrityError error = ContentIntegrityError.createError(node, null, msg, this);
+                        error.setExtraInfos(ErrorType.DIFFERENT_PATH);
+                        return error;
+                    }
+                }
             }
             return null;
         } catch (RepositoryException e) {
             logger.error("", e);
             return null;
+        }
+    }
+
+    private boolean hasPendingModifications(Node node) {
+        try {
+            if (!node.isNodeType(JAHIAMIX_LASTPUBLISHED)) return false;
+            if (!node.hasProperty(LASTPUBLISHED)) return true;
+            final Calendar lastPublished = node.getProperty(LASTPUBLISHED).getDate();
+            if (lastPublished == null) return true;
+            if (!node.hasProperty(JCR_LASTMODIFIED)) {
+                // If this occurs, then it should be detected by another integrityCheck. But here there's no way to deal with such node.
+                logger.error("The node has no last modification date set " + node.getPath());
+                return false;
+            }
+            final Calendar lastModified = node.getProperty(JCR_LASTMODIFIED).getDate();
+
+            return lastModified.after(lastPublished);
+        } catch (RepositoryException e) {
+            logger.error("", e);
+            // If we can't validate that there's some pending modifications here, then we assume that there are no one.
+            return false;
         }
     }
 
