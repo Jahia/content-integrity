39fc0bd0073a166f3a2220f8722b8e78a4cbb3b8
Damien <dsaulnier@jahia.com>
2018-05-18 12:56:09 +0200

    Moved "print registered checks" to a dedicated command

--- src/main/java/org/jahia/modules/verifyintegrity/jcrcommands/CheckIntegrityCommand.java
+++ src/main/java/org/jahia/modules/verifyintegrity/jcrcommands/CheckIntegrityCommand.java
@@ -3,7 +3,6 @@ package org.jahia.modules.verifyintegrity.jcrcommands;
 import org.apache.commons.lang.StringUtils;
 import org.apache.karaf.shell.api.action.Action;
 import org.apache.karaf.shell.api.action.Command;
-import org.apache.karaf.shell.api.action.Completion;
 import org.apache.karaf.shell.api.action.Option;
 import org.apache.karaf.shell.api.action.lifecycle.Reference;
 import org.apache.karaf.shell.api.action.lifecycle.Service;
@@ -11,29 +10,18 @@ import org.apache.karaf.shell.api.console.Session;
 import org.jahia.modules.verifyintegrity.services.ContentIntegrityResults;
 import org.jahia.modules.verifyintegrity.services.Utils;
 
-@Command(scope = "jcr", name = "integrity-check")
+@Command(scope = "jcr", name = "integrity-check", description = "Runs an integrity scan")
 @Service
 public class CheckIntegrityCommand extends JCRCommandSupport implements Action {
 
     @Reference
     Session session;
 
-    @Option(name = "-l", aliases = "--limit", description = "Maximum number of lines to print.")
+    @Option(name = "-l", aliases = "--limit", description = "Maximum number of lines to print")
     private String limit;
-
-    @Option(name = "-lc", aliases = "--listChecks", description = "List the registered checks.")
-    @Completion(OutputLevelCompleter.class)
-    private String listChecks;
 
     @Override
     public Object execute() throws Exception {
-        if (listChecks != null) {
-            final boolean fullOutput = "full".equalsIgnoreCase(listChecks);
-            for (String s : Utils.getContentIntegrityService().printIntegrityChecksList(!fullOutput)) {
-                System.out.println(s);
-            }
-            return null;
-        }
         final String currentPath = StringUtils.defaultString(getCurrentPath(session), "/");
         final ContentIntegrityResults integrityResults = Utils.getContentIntegrityService().validateIntegrity(currentPath, getCurrentWorkspace(session));
         printContentIntegrityErrors(integrityResults, limit);
--- src/main/java/org/jahia/modules/verifyintegrity/jcrcommands/OutputLevelCompleter.java
+++ src/main/java/org/jahia/modules/verifyintegrity/jcrcommands/OutputLevelCompleter.java
@@ -6,8 +6,6 @@ import org.apache.karaf.shell.api.console.CommandLine;
 import org.apache.karaf.shell.api.console.Completer;
 import org.apache.karaf.shell.api.console.Session;
 import org.apache.karaf.shell.support.completers.StringsCompleter;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
@@ -15,15 +13,16 @@ import java.util.List;
 @Service
 public class OutputLevelCompleter implements Completer {
 
+    static final List<String> LEVELS = Arrays.asList("simple", "full");
+
     @Override
     public int complete(Session session, CommandLine commandLine, List<String> candidates) {
-        final List<String> levels = Arrays.asList("simple", "full");
         final StringsCompleter delegate = new StringsCompleter();
         final String argument = commandLine.getCursorArgument();
         if (StringUtils.isBlank(argument)) {
-            candidates.addAll(levels);
+            candidates.addAll(LEVELS);
         } else {
-            for (String date : levels) {
+            for (String date : LEVELS) {
                 if (date.startsWith(argument)) delegate.getStrings().add(date);
             }
         }
