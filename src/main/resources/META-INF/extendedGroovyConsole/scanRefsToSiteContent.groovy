a16981b979c5ef654d52c51d4262bbb3edbe905b
dsaulnier <dsaulnier@jahia.com>
2022-02-15 19:07:31 +0100

    Added the possibility to hide the known false positives

--- src/main/resources/META-INF/extendedGroovyConsole/scanRefsToSiteContent.groovy
+++ src/main/resources/META-INF/extendedGroovyConsole/scanRefsToSiteContent.groovy
@@ -8,12 +8,13 @@ import org.jahia.services.content.JCRTemplate
 import javax.jcr.Property
 import javax.jcr.RepositoryException
 
+boolean hide = hideFP
 if (Arrays.asList("all", "default").contains(workspace))
-    scanSite(site, Constants.EDIT_WORKSPACE)
+    scanSite(site, Constants.EDIT_WORKSPACE, hide)
 if (Arrays.asList("all", "live").contains(workspace))
-    scanSite(site, Constants.LIVE_WORKSPACE)
+    scanSite(site, Constants.LIVE_WORKSPACE, hide)
 
-void scanSite(String siteNodeID, String ws) {
+void scanSite(String siteNodeID, String ws, boolean hideReferencesToIgnore) {
 
     log.info("Running on the workspace " + ws)
     log.info(" ------------------------------------")
@@ -24,21 +25,31 @@ void scanSite(String siteNodeID, String ws) {
 
             JCRNodeWrapper siteNode = session.getNodeByIdentifier(siteNodeID)
             String sitePath = siteNode.getPath()
-            scan(siteNode, sitePath)
+            scan(siteNode, sitePath, hideReferencesToIgnore)
             return null
         }
 
-        void scan(JCRNodeWrapper node, String sitePath) {
+        void scan(JCRNodeWrapper node, String sitePath, boolean hideRefsToIgnore) {
             for (Property p : node.getWeakReferences()) {
                 String path = p.getPath()
-                if (!path.startsWith(sitePath)) {
+                if (!path.startsWith(sitePath) && logReference(path, node, hideReferencesToIgnore)) {
                     log.info(path + " -> " + node.getPath())
                 }
             }
 
             for (JCRNodeWrapper child : node.getNodes()) {
-                scan(child, sitePath)
+                scan(child, sitePath, hideReferencesToIgnore)
             }
+        }
+
+        boolean logReference(String propertyPath, JCRNodeWrapper targetNode, boolean hideRefsToIgnore) {
+            if (!hideReferencesToIgnore) return true
+
+            if (StringUtils.equals(propertyPath, "/sites/j:defaultSite")) return false
+            if (StringUtils.equals(propertyPath, String.format("/groups/privileged/j:members%s/j:member", targetNode.getPath()))
+                    && StringUtils.equals(targetNode.getName(), "site-privileged")) return false
+
+            return true
         }
     })
 
