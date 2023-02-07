6254b69ba14ee73f06df4ba8887e454628cf2969
dsaulnier <dsaulnier@jahia.com>
2023-02-07 14:33:00 +0100

    NodeNameInfoSanityCheck : - improved the detection of the real errors - excluded some false positives - make the check active by default, since the error detection is finalized now

--- src/main/java/org/jahia/modules/contentintegrity/services/checks/NodeNameInfoSanityCheck.java
+++ src/main/java/org/jahia/modules/contentintegrity/services/checks/NodeNameInfoSanityCheck.java
@@ -5,6 +5,7 @@ import org.jahia.api.Constants;
 import org.jahia.modules.contentintegrity.api.ContentIntegrityCheck;
 import org.jahia.modules.contentintegrity.api.ContentIntegrityErrorList;
 import org.jahia.modules.contentintegrity.services.impl.AbstractContentIntegrityCheck;
+import org.jahia.modules.contentintegrity.services.impl.JCRUtils;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.osgi.service.component.annotations.Component;
 import org.slf4j.Logger;
@@ -21,20 +22,55 @@ public class NodeNameInfoSanityCheck extends AbstractContentIntegrityCheck {
 
     private static final Logger logger = LoggerFactory.getLogger(NodeNameInfoSanityCheck.class);
 
-    private enum ErrorType {MISSING_FULLPATH, INVALID_FULLPATH, MISSING_NODENAME, INVALID_NODENAME}
+    private enum ErrorType {MISSING_FULLPATH, UNEXPECTED_FULLPATH, INVALID_FULLPATH, MISSING_NODENAME, INVALID_NODENAME}
 
     @Override
     public ContentIntegrityErrorList checkIntegrityBeforeChildren(JCRNodeWrapper node) {
         final ContentIntegrityErrorList errors = createEmptyErrorsList();
 
         try {
-            validateStringProperty(node, Constants.FULLPATH, JCRNodeWrapper::getPath, errors, ErrorType.MISSING_FULLPATH, ErrorType.INVALID_FULLPATH);
+            if (JCRUtils.isInDefaultWorkspace(node)) {
+                if (JCRUtils.isNeverPublished(node)) {
+                    validateMissingFullpathProperty(node, errors);
+                } else if (!JCRUtils.hasPendingModifications(node)) {
+                    validateConsistentFullpathProperty(node, errors);
+                } else {
+                    // In this case, we expect the property to hold the path of the node in live, since it was the path the last time the node was published
+                    validateStringProperty(node, Constants.FULLPATH, defaultNode -> {
+                        try {
+                            return JCRUtils.getSystemSession(Constants.LIVE_WORKSPACE).getNodeByUUID(defaultNode.getIdentifier()).getPath();
+                        } catch (RepositoryException e) {
+                            logger.error("", e);
+                            return null; // TODO : this should never happen, but if it does, the resulting behavior in uncontrolled
+                        }
+                    }, errors, ErrorType.MISSING_FULLPATH, ErrorType.INVALID_FULLPATH);
+                }
+            } else {
+                if (JCRUtils.isUGCNode(node)) {
+                    validateMissingFullpathProperty(node, errors);
+                } else {
+                    validateConsistentFullpathProperty(node, errors);
+                }
+            }
             validateStringProperty(node, Constants.NODENAME, JCRNodeWrapper::getName, errors, ErrorType.MISSING_NODENAME, ErrorType.INVALID_NODENAME);
         } catch (RepositoryException e) {
             logger.error("", e);
         }
 
         return errors;
+    }
+
+    private void validateMissingFullpathProperty (JCRNodeWrapper node, ContentIntegrityErrorList errors) throws RepositoryException {
+        // TODO : the message should be different for a never published node in default and a UGC node in live
+        if (node.hasProperty(Constants.FULLPATH)) {
+            errors.addError(createError(node, String.format("Unexpected %s property", Constants.FULLPATH))
+                    .setErrorType(ErrorType.UNEXPECTED_FULLPATH)
+                    .setExtraMsg("The property is expected to be missing on never published nodes in the default workspace and on UGC nodes in the live workspace"));
+        }
+    }
+
+    private void validateConsistentFullpathProperty (JCRNodeWrapper node, ContentIntegrityErrorList errors) throws RepositoryException {
+        validateStringProperty(node, Constants.FULLPATH, JCRNodeWrapper::getPath, errors, ErrorType.MISSING_FULLPATH, ErrorType.INVALID_FULLPATH);
     }
 
     private void validateStringProperty(JCRNodeWrapper node, String propertyName,
