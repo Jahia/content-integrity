1cc179d5a1dbc27654d4c7013e0adfd4ecc8a789
dsaulnier <dsaulnier@jahia.com>
2023-04-12 10:13:57 +0200

    Always use JCRUtils to access the current system session

--- src/main/java/org/jahia/modules/contentintegrity/services/ContentIntegrityServiceImpl.java
+++ src/main/java/org/jahia/modules/contentintegrity/services/ContentIntegrityServiceImpl.java
@@ -18,7 +18,6 @@ import org.jahia.modules.contentintegrity.services.impl.JCRUtils;
 import org.jahia.modules.contentintegrity.services.util.ProgressMonitor;
 import org.jahia.services.SpringContextSingleton;
 import org.jahia.services.cache.ehcache.EhCacheProvider;
-import org.jahia.services.content.JCRNodeIteratorWrapper;
 import org.jahia.services.content.JCRNodeWrapper;
 import org.jahia.services.content.JCRSessionFactory;
 import org.jahia.services.content.JCRSessionWrapper;
@@ -36,11 +35,9 @@ import javax.jcr.RepositoryException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.concurrent.Semaphore;
@@ -149,13 +146,9 @@ public class ContentIntegrityServiceImpl implements ContentIntegrityService {
 
         try {
             JCRSessionFactory.getInstance().closeAllSessions();
-            final JCRSessionWrapper session;
-            try {
-                session = getSystemSession(workspace);
-            } catch (RepositoryException e) {
-                logger.error(String.format("Impossible to get the session for workspace %s", workspace), e);
-                return null;
-            }
+            final JCRSessionWrapper session = JCRUtils.getSystemSession(workspace);
+            if (session == null) return null;
+
             try {
                 final JCRNodeWrapper node = session.getNode(path);
                 final String excludedPathsDesc = CollectionUtils.isNotEmpty(excludedPaths) ?
@@ -461,7 +454,6 @@ public class ContentIntegrityServiceImpl implements ContentIntegrityService {
     }
 
     public void fixErrors(List<ContentIntegrityError> errors) {
-        final Map<String, JCRSessionWrapper> sessions = new HashMap<>();
         for (ContentIntegrityError error : errors) {
             final ContentIntegrityCheck integrityCheck = getContentIntegrityCheck(error.getIntegrityCheckID());
             if (integrityCheck == null) {
@@ -471,16 +463,9 @@ public class ContentIntegrityServiceImpl implements ContentIntegrityService {
             if (!(integrityCheck instanceof ContentIntegrityCheck.SupportsIntegrityErrorFix)) continue;
 
             final String workspace = error.getWorkspace();
-            JCRSessionWrapper session = sessions.get(workspace);
-            if (session == null) {
-                try {
-                    session = getSystemSession(workspace);
-                } catch (RepositoryException e) {
-                    logger.error(String.format("Impossible to get the session for workspace %s", workspace), e);
-                    continue;
-                }
-                sessions.put(workspace, session);
-            }
+            final JCRSessionWrapper session = JCRUtils.getSystemSession(workspace);
+            if (session == null) continue;
+
             try {
                 final String uuid = error.getUuid();
                 final JCRNodeWrapper node = session.getNodeByUUID(uuid);
@@ -491,10 +476,6 @@ public class ContentIntegrityServiceImpl implements ContentIntegrityService {
                 logger.error(String.format("Failed to fix the error %s", error.toJSON()), e);
             }
         }
-    }
-
-    private JCRSessionWrapper getSystemSession(String workspace) throws RepositoryException {
-        return JCRSessionFactory.getInstance().getCurrentSystemSession(workspace, null, null);
     }
 
     @Override
